f49a48a3bc7fb770ce5913120c502b6ff7161ba2
Uzayisenga <aminauzayisenga@gmail.com>
2021-03-04 14:08:17 +0200

    first commit

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,11 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
+
+//= require jquery_ujs
 //= require activestorage
 //= require turbolinks
+//= require jquery
 //= require bootstrap-sprockets
 //= require toastr
 //= require_tree .
